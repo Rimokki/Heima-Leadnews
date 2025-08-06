1452f6d9c9aee7a1df630769e80f6ccaa8b65ba4
Rimokki <rimokki@outlook.com>
2025-08-06 19:06:11 +0800

    添加axios拦截器

--- utils/request.js
+++ utils/request.js
@@ -1,3 +1,24 @@
 // axios 公共配置
 // 基地址
 axios.defaults.baseURL = 'http://geek.itheima.net'
+// 请求拦截器
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('token')
+  token && (config.headers.Authorization = `Bearer ${token}`)
+  return config
+})
+// 响应拦截器
+axios.interceptors.response.use(
+  function (response) {
+    return response.data
+  },
+  function (error) {
+    console.dir(error)
+    if (error?.response?.status === 401) {
+      alert('登录状态过期，请重新登录')
+      localStorage.clear()
+      location.href = '../login/index.html'
+    }
+    return Promise.reject(error)
+  }
+)
